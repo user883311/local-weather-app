d632fc07a093c05c894371e28053c40c6a31b3b5
user883311 <tarik.chardon@gmail.com>
2018-05-24 22:46:16 +0800

    fix

--- script.js
+++ script.js
@@ -1,108 +1,108 @@
-// var lon, lat;
-// $(document).ready(function () {
-//     navigator.geolocation.getCurrentPosition(function (location) {
-//         lon = location.coords.longitude;
-//         lat = location.coords.latitude;
-//         console.log(lon, lat);
-//         getWeather(lon, lat);
-//     });
-// })
+    // var lon, lat;
+    // $(document).ready(function () {
+    //     navigator.geolocation.getCurrentPosition(function (location) {
+    //         lon = location.coords.longitude;
+    //         lat = location.coords.latitude;
+    //         console.log(lon, lat);
+    //         getWeather(lon, lat);
+    //     });
+    // })
 
-//Check if browser supports W3C Geolocation API
-// if (navigator.geolocation) {
-//     navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
-// }
-// //Get latitude and longitude;
-// function successFunction(position) {
-//     var lat = position.coords.latitude;
-//     var long = position.coords.longitude;
-//     console.log(lat, long);
-//     getWeather(lat, long);
-// }
+    //Check if browser supports W3C Geolocation API
+    // if (navigator.geolocation) {
+    //     navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
+    // }
+    // //Get latitude and longitude;
+    // function successFunction(position) {
+    //     var lat = position.coords.latitude;
+    //     var long = position.coords.longitude;
+    //     console.log(lat, long);
+    //     getWeather(lat, long);
+    // }
 
-// function errorFunction(error) {
-//     console.log(error);
-// }
+    // function errorFunction(error) {
+    //     console.log(error);
+    // }
 
-// function getWeather(lon, lat) {
-//     // https://openweathermap.org/current
-//     let api = new XMLHttpRequest();
-//     // let APIKEY = "f13e33dc5e96b1a4c9fc0948f9095d1a"
-//     api.open("GET", `http://gd.geobytes.com/GetCityDetails`);
-//     // api.setRequestHeader(
-//     //     "Accept", "application/json",
-//     //     "Content-Type", "application/x-www-form-urlencoded"
-//     // )
-//     api.send();
-//     api.onreadystatechange = function () {
-//         if (this.readyState == 4 && this.status == 200) {
-//             console.log(this);
-//             var data = JSON.parse(this.response);
-//             console.log(data);
-//         }
-//     }
-// }
-
-
-
-// function getWeather(lon, lat) {
-//     var weatherAPI = "https://fcc-weather-api.glitch.me/";
-//     weatherAPI += "/api/current?lon=" + lon;
-//     weatherAPI += "&lat=" + lat;
-//     var JSONcity, JSONcountryCode, JSONicon;
-//     var JSONdata, JSONweatherDescription, JSONtemperature, JSONhumidity, JSONwindspeed;
-//     $.ajax({
-//         type: "GET",
-//         url: weatherAPI,
-//         success: function (data) {
-//             //console.log(data);
-//             JSONdata = data;
-//             document.getElementById("location-box").innerHTML = data.name + ", " + data.sys.country;
-//             JSONicon = data.weather[0].icon;
-//             //window.open(weatherAPI);
-//             document.getElementById("icon-box").innerHTML = "<img src='" + JSONicon + "' alt='[an API weather image]'>";
-//             document.getElementById("weather-description-box").innerHTML = data.weather[0].description;
-//             document.getElementById("temperature-box").innerHTML = Math.round(data.main.temp);
-//             document.getElementById("humidity-box").innerHTML = data.main.humidity + " % humidity";
-//             document.getElementById("wind-speed-box").innerHTML = "Wind speed: " + data.wind.speed + " km/h";
-//         }
-//     });
-// };
-
-
-// $(document).ready(function () {
-//     $("#fahrenheit").click(function () {
-//         document.getElementById("temperature-box").innerHTML = Math.round(document.getElementById("temperature-box").innerHTML * 9 / 5 + 32);
-//     });
-// });
-// $(document).ready(function () {
-//     $("#celsius").click(function () {
-//         document.getElementById("temperature-box").innerHTML = Math.round((document.getElementById("temperature-box").innerHTML - 32) * 5 / 9);
-//     });
-// });
+    // function getWeather(lon, lat) {
+    //     // https://openweathermap.org/current
+    //     let api = new XMLHttpRequest();
+    //     // let APIKEY = "f13e33dc5e96b1a4c9fc0948f9095d1a"
+    //     api.open("GET", `http://gd.geobytes.com/GetCityDetails`);
+    //     // api.setRequestHeader(
+    //     //     "Accept", "application/json",
+    //     //     "Content-Type", "application/x-www-form-urlencoded"
+    //     // )
+    //     api.send();
+    //     api.onreadystatechange = function () {
+    //         if (this.readyState == 4 && this.status == 200) {
+    //             console.log(this);
+    //             var data = JSON.parse(this.response);
+    //             console.log(data);
+    //         }
+    //     }
+    // }
 
 
 
-// getWeather(1,130);
+    // function getWeather(lon, lat) {
+    //     var weatherAPI = "https://fcc-weather-api.glitch.me/";
+    //     weatherAPI += "/api/current?lon=" + lon;
+    //     weatherAPI += "&lat=" + lat;
+    //     var JSONcity, JSONcountryCode, JSONicon;
+    //     var JSONdata, JSONweatherDescription, JSONtemperature, JSONhumidity, JSONwindspeed;
+    //     $.ajax({
+    //         type: "GET",
+    //         url: weatherAPI,
+    //         success: function (data) {
+    //             //console.log(data);
+    //             JSONdata = data;
+    //             document.getElementById("location-box").innerHTML = data.name + ", " + data.sys.country;
+    //             JSONicon = data.weather[0].icon;
+    //             //window.open(weatherAPI);
+    //             document.getElementById("icon-box").innerHTML = "<img src='" + JSONicon + "' alt='[an API weather image]'>";
+    //             document.getElementById("weather-description-box").innerHTML = data.weather[0].description;
+    //             document.getElementById("temperature-box").innerHTML = Math.round(data.main.temp);
+    //             document.getElementById("humidity-box").innerHTML = data.main.humidity + " % humidity";
+    //             document.getElementById("wind-speed-box").innerHTML = "Wind speed: " + data.wind.speed + " km/h";
+    //         }
+    //     });
+    // };
 
-// function getWeather(lon, lat) {
-//     // https://darksky.net/
-//     let api = new XMLHttpRequest();
-//     let APIKEY = "020ccb1cb7edea9f2d828997f73b49fd";
-//     api.open("GET", `https://api.darksky.net/forecast/020ccb1cb7edea9f2d828997f73b49fd/1,130`);
-//     api.setRequestHeader(
-//         "Accept", "application/json",
-//         "crossDomain", true
-//     )
-//     api.send();
-//     api.onreadystatechange = function () {
-//         if (this.readyState == 4 && this.status == 200) {
-//             console.log(this);
-//             var data = JSON.parse(this.response);
-//             console.log(data);
-//         }
-//     }
-// }
+
+    // $(document).ready(function () {
+    //     $("#fahrenheit").click(function () {
+    //         document.getElementById("temperature-box").innerHTML = Math.round(document.getElementById("temperature-box").innerHTML * 9 / 5 + 32);
+    //     });
+    // });
+    // $(document).ready(function () {
+    //     $("#celsius").click(function () {
+    //         document.getElementById("temperature-box").innerHTML = Math.round((document.getElementById("temperature-box").innerHTML - 32) * 5 / 9);
+    //     });
+    // });
+
+
+
+    // getWeather(1,130);
+
+    // function getWeather(lon, lat) {
+    //     // https://darksky.net/
+    //     let api = new XMLHttpRequest();
+    //     let APIKEY = "020ccb1cb7edea9f2d828997f73b49fd";
+    //     api.open("GET", `https://api.darksky.net/forecast/020ccb1cb7edea9f2d828997f73b49fd/1,130`);
+    //     api.setRequestHeader(
+    //         "Accept", "application/json",
+    //         "crossDomain", true
+    //     )
+    //     api.send();
+    //     api.onreadystatechange = function () {
+    //         if (this.readyState == 4 && this.status == 200) {
+    //             console.log(this);
+    //             var data = JSON.parse(this.response);
+    //             console.log(data);
+    //         }
+    //     }
+    // }
 
 
 
@@ -127,8 +127,7 @@ function createCORSRequest(method, url) {
 // Make the actual CORS request.
 function makeCorsRequest() {
     // This is a sample server that supports CORS.
-    var url = 'https://api.darksky.net/forecast/020ccb1cb7edea9f2d828997f73b49fd/1,130';
-
+    var url = 'https://api.darksky.net/forecast/020ccb1cb7edea9f2d828997f73b49fd/42.3601,-71.0589';
     var xhr = createCORSRequest('GET', url);
     
     if (!xhr) {
